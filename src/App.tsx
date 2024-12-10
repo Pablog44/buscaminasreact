569958fe9aee1462a58acf549987bbac36769053
PABLO GONZALEZ CUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-12-10 15:08:41 +0100

    se creo
    Nuevo proyecto

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const Home = () => <div>Home Tab</div>;
+const About = () => <div>About Tab</div>;
+const Contact = () => <div>Contact Tab</div>;
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
